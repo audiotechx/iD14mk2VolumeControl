fd2235a27585dff04e3a6b16f255b556b0120725
audiotechx <276073997+audiotechx@users.noreply.github.com>
2026-08-20 00:19:37 +0500

    Update iDVolume_MultiLaunch.bat

--- iDVolume_MultiLaunch.bat
+++ iDVolume_MultiLaunch.bat
@@ -1,14 +1,12 @@
 REM EXAMPLES
 
 
-
 REM 1. Recommended:
 
 @echo off
 start /wait "" "%~dp0iDVolume.exe" mon 50
 start /wait "" "%~dp0iDVolume.exe" hp 65
 exit
-
 
 
 REM 2. Quickly and simultaneously:

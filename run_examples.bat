6aa762061b6b4b7615b562d314d39287555c8617
audiotechx <276073997+audiotechx@users.noreply.github.com>
2026-08-26 01:10:44 +0500

    Add files via upload

---
+++ run_examples.bat
@@ -0,0 +1,17 @@
+REM EXAMPLES
+
+
+REM 1. Recommended:
+
+@echo off
+start /wait "" "%~dp0iDVolume.exe" mon 50
+start /wait "" "%~dp0iDVolume.exe" hp 65
+exit
+
+
+REM 2. Quickly and simultaneously:
+
+@echo off
+start "" "%~dp0iDVolume.exe" mon 50
+start "" "%~dp0iDVolume.exe" hp 65
+exit
